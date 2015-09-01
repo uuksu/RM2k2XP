586c6cfccc385d108cca00a4dfa620789396e2cb
Mikko Uuksulainen <mikko.uuksulainen@gmail.com>
2015-09-01 12:40:26 +0300

    Added CommandLineOptions to main project so it is possible to use converter from commnad line.

---
+++ RM2k2XP/CommandLineOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace RM2k2XP
+{
+    class CommandLineOptions
+    {
+        [ValueList(typeof(List<string>), MaximumElements = 1)]
+        public IList<string> InputPaths { get; set; }
+
+        [Option('t', "type", Required = true, HelpText = "Source resource type")]
+        public string Type { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            Assembly entryAssembly = Assembly.GetEntryAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(entryAssembly.Location);
+
+            HelpText help = new HelpText
+            {
+                Heading = new HeadingInfo("RM2k2XP", fvi.FileVersion),
+                Copyright = new CopyrightInfo("Mikko Uuksulainen", 2015),
+                AdditionalNewLineAfterOption = true,
+                AddDashesToOption = true
+            };
+
+            help.AddPreOptionsLine(Environment.NewLine);
+            help.AddPreOptionsLine("Usage: RM2k2XP <inputPath> [options]");
+            help.AddOptions(this);
+
+            return help;
+        }
+    }
+}
--- RM2k2XP/Program.cs
+++ RM2k2XP/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 using RM2k2XP.Converters;
 using RM2k2XP.Converters.Formats;
 
@@ -10,14 +12,51 @@ namespace RM2k2XP
 {
     class Program
     {
+        private static CommandLineOptions _commandLineOptions;
+
         static void Main(string[] args)
         {
+            _commandLineOptions = new CommandLineOptions();
+
+            if (Parser.Default.ParseArguments(args, _commandLineOptions) == false)
+            {
+                Environment.Exit(1);
+            }
+
+            if (_commandLineOptions.InputPaths.Count == 0)
+            {
+                Console.WriteLine("Please provide input path.");
+                Environment.Exit(1);
+            }
+
+            switch (_commandLineOptions.Type)
+            {
+                case "charset":
+                    ProcessCharsetConversion();
+                    break;
+                default:
+                    Console.WriteLine("Unknown resource type.");
+                    Environment.Exit(1);
+                    break;
+            }
+        }
+
+        private static void ProcessCharsetConversion()
+        {
+            FileInfo inputFileInfo = new FileInfo(_commandLineOptions.InputPaths.First());
+
+            if (!inputFileInfo.Exists)
+            {
+                Console.WriteLine("Input file does not exists.");
+                Environment.Exit(1);
+            }
+
             RPGMaker2000CharsetConverter converter = new RPGMaker2000CharsetConverter();
-            List<RPGMakerXPCharset> charsets = converter.ToRPGMakerXpCharset("hero1.png");
+            List<RPGMakerXPCharset> charsets = converter.ToRPGMakerXpCharset(inputFileInfo.FullName);
 
             for (int i = 0; i < charsets.Count; i++)
             {
-                charsets[i].Save(i.ToString());
+                charsets[i].Save(String.Format("{0}_{1}", Path.GetFileNameWithoutExtension(inputFileInfo.FullName), i));
             }
         }
     }
