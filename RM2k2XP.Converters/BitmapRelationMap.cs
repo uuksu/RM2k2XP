5ac774dc5c543077a2bdb309c6f945e170b4058a
Mikko Uuksulainen <mikko.uuksulainen@gmail.com>
2015-09-01 15:46:39 +0300

    Added dummy bitmap relation map list.

---
+++ RM2k2XP.Converters/BitmapRelationMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM2k2XP.Converters
+{
+    /// <summary>
+    /// Represents mapping information between two bitmaps.
+    /// </summary>
+    class BitmapRelationMap
+    {
+        /// <summary>
+        /// Rectangle in destination bitmap.
+        /// </summary>
+        public Rectangle DestinationRectangle { get; set; }
+
+        /// <summary>
+        /// Rectangle in source bitmap.
+        /// </summary>
+        public Rectangle SourceRectangle { get; set; }
+    }
+}
--- RM2k2XP.Converters/RPGMaker2000ChipsetConverter.cs
+++ RM2k2XP.Converters/RPGMaker2000ChipsetConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,13 @@ namespace RM2k2XP.Converters
 {
     public class RPGMaker2000ChipsetConverter
     {
+        private List<BitmapRelationMap> bitmapRelationMaps = new List<BitmapRelationMap>
+        {
+            new BitmapRelationMap { SourceRectangle = new Rectangle(0, 0, 16, 16), DestinationRectangle = new Rectangle(0, 0, 16, 16) },
+            new BitmapRelationMap { SourceRectangle = new Rectangle(0, 48, 16, 16), DestinationRectangle = new Rectangle(32, 0, 16, 16) },
+            new BitmapRelationMap { SourceRectangle = new Rectangle(0, 0, 8, 8), DestinationRectangle = new Rectangle(0, 16, 8, 8) }
+        };
+
         public RPGMakerXPTileset ToRPGMakerXpTileset(string inputPath)
         {
             throw new NotImplementedException();
