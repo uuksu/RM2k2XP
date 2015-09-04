a2aceab10ee51291f6c9885cb8e28e9c5e4e7bea
Mikko Uuksulainen <mikko.uuksulainen@gmail.com>
2015-09-04 10:34:09 +0300

    Added icons for Gui and Cli.

--- RM2k2XP.Gui/MainForm.Designer.cs
+++ RM2k2XP.Gui/MainForm.Designer.cs
@@ -300,6 +300,7 @@
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
